f2685e37d855a2813b5303b0b9fc638ebe8aa868
Damilare Adegboye <damilareadegboye2000@gmail.com>
2022-07-01 23:45:38 +0100

    this is my project on classes and object

---
+++ main.py
@@ -0,0 +1,26 @@
+class Student:    
+    def __init__(self, name, tracks, age, score):
+        self.name = name
+        self.age = int(age)
+        self.tracks = tracks
+        self.score = score
+    
+    def change_name(self, name):
+        self.name = name
+    
+    def change_age(self, age):
+        self.age = int(age)
+    
+    def add_track(self, track):
+        self.tracks.append(track)
+    
+    def get_score(self):
+        return self.score
+
+Bob = Student(name="Bob", age=26, tracks=["FE", "BE"], score=20.90)
+
+# Expected methods
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score()
